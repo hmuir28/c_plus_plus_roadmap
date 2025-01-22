39f972c9631f6911398f6b7d0832a80656c2a6b9
Harry Muir Camacho <hmuir@Harrys-MBP.lan>
2025-01-23 00:11:50 +0100

    Added gitignore and typedef module

---
+++ typedef/typedef.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+using pairlist_t = std::vector<std::pair<std::string, int>>;
+
+int main() {
+
+    pairlist_t pairs;
+
+    
+
+    return 0;
+}
